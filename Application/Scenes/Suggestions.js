cbfdc147c22e0165b178d7095598d78588edf658
aesthetism <zman97619@gmail.com>
2019-02-06 12:12:01 +0530

    suggestions

--- Application/Scenes/Suggestions.js
+++ Application/Scenes/Suggestions.js
@@ -23,11 +23,10 @@ export default class Suggestions extends React.Component {
             style={styles.suglist}
               data={[
                 {'key':'Check the Rice crop'},
-                {'key':'Check the Rice crop'},
-                {'key':'Check the Rice crop'},
-                {'key':'Check the Rice crop'},
-                {'key':'Check the Rice crop'},
-                {'key':'Check the Rice crop'},
+                {'key':'Ensure Proper Water Drainage'},
+                {'key':'Fertilizing your corn at the time of seeding can help provide the seeds with essential nutrients like potassium, phosphorous, and calcium.'},
+                {'key':'Developing a drainage system in your crops can help prevent waterlogging and salinization in your soil'},
+                {'key':'suggestion..'}
               ]}
               renderItem={({item})=>
               <View style={styles.sugitem}>
